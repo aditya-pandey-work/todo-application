0a0d2c804a52e72bf6821416aa37c380de5bdb60
aditya <aditya.work@yahoo.com>
2026-02-09 13:57:48 +0530

    connected posgre db of docker hub, used docker compose to run 2 docker container

--- dependencies.py
+++ dependencies.py
@@ -1,13 +1,14 @@
 from db import SessionLocal
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 from db import SessionLocal
 from models import User
 from auth import SECRET_KEY, ALGORITHM
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+security=HTTPBearer()
 
 def get_db():
     database = SessionLocal()
@@ -17,12 +18,13 @@ def get_db():
         database.close()
 
 def get_current_user(
-        token: str = Depends(oauth2_scheme), 
+        credentials: HTTPAuthorizationCredentials = Depends(security), 
         db: Session = Depends(get_db)
 ):
     try:
+        token=credentials.credentials
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
+        user_id = int(payload.get("sub"))
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
